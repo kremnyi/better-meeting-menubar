9dcae23eceb87a80a9012de202af3f65ff57fa56
Bohdan Kremnyi <b.kremnyi@gmail.com>
2026-09-07 10:18:13 +0200

    Release Better Meeting 0.3.18

--- Casks/better-meeting.rb
+++ Casks/better-meeting.rb
@@ -1,6 +1,6 @@
 cask "better-meeting" do
-  version "0.3.17"
-  sha256 "b84e354b809ea97edbdcea5f99b9d7ed1cea953e0bb599934429f1229b8f3225"
+  version "0.3.18"
+  sha256 "ffbcd51a26c8356db11c8baaecb4bbd002341c606f9aebe156d8fc5a658d1d91"
 
   url "https://github.com/kremnyi/better-meeting/releases/download/v#{version}/Better-Meeting-#{version}-arm64.zip"
   name "Better Meeting"
